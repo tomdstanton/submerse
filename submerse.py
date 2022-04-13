b3d8bd4e2cea288b0c441b0fc37af3c6807f84f4
Tom Stanton <tomdstanton@gmail.com>
2022-04-13 21:20:34 +0100

    Bump to 0.0.3, change/add equations, remove multithreading for TODO

--- submerse.py
+++ submerse.py
@@ -13,6 +13,7 @@ Tom Stanton, 2022
 """
 
 # ............... Imports ............... #
+import os
 from random import seed, choices
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import gzip
@@ -22,7 +23,7 @@ from os import cpu_count, path, makedirs
 from re import compile, IGNORECASE, search, findall
 from itertools import islice
 from time import time
-
+from concurrent.futures import ProcessPoolExecutor, wait
 
 # ............... Attributes ............... #
 __version__ = '0.0.3'
@@ -46,51 +47,6 @@ read_regex = compile('|'.join([
     '\.(f(?:ast)?q(?:\.gz)?)$']), IGNORECASE)
 
 logger = logging.getLogger(__name__)  # https://stackoverflow.com/a/45065655/10771025
-
-
-# ............... Main Program ............... #
-def main():
-    args = parse_args(sys.argv[1:])
-    # args = parse_args("-a /Users/tom/Bioinformatics/submerse/R-AJ101A_S62_L002.fasta
-    # -1 /Users/tom/Bioinformatics/submerse/R-AJ101A_S62_L002_1.fastq.gz
-    # -2 /Users/tom/Bioinformatics/submerse/R-AJ101A_S62_L002_2.fastq.gz -f".split())
-
-    if not any([args.forward, args.reverse, args.single]):
-        quit_with_error('No fastq files provided')
-
-    if args.force and len(args.assembly) not in [len(i) for i in [args.forward, args.reverse, args.single] if i]:
-        quit_with_error('The --force argument requires the number of reads in at least one of the read sets to be the '
-                        'same as the number of assemblies')
-
-    if args.out and not args.depths:
-        quit_with_error('Can only output sub-sampled reads at selected depths with the -d option')
-
-    if args.out and not args.random_seed:
-        quit_with_error('Can only randomly sub-sample reads with a random seed, try -r 100')
-
-    seed(args.random_seed)  # Set random seed using the args
-
-    line_width = 70  # Nice general terminal width
-    logging.basicConfig(format="[%(asctime)s %(levelname)s %(funcName)s] %(message)s",
-                        datefmt='%H:%M:%S', level=args.loglevel)
-    logger.info(format_str(f' {path.basename(__file__)} {__version__} '.center(line_width, '='), '36'))
-    logger.info(f'Platform: {sys.platform}')
-    logger.info(f'Python: {".".join([str(i) for i in sys.version_info[:3]])}')
-
-    samples = process_input(args.forward, args.reverse, args.single, args.assembly, args.force)
-
-    header = f'Sample\tAssembly_file\tGenome_size\tTotal_reads\tTotal_bases\tAve_mean_read_length\tAve_mode_read_length\t' \
-             f'Total_coverage\tRead_file\tMean_read_length\tMode_read_length\tRead_coverage\tMax_read_length\t' \
-             f'Min_read_length\tN_reads\tN_bases'
-    if args.depths:
-        header += '\t' + "\t".join([f"N_reads_at_{depth}X_depth" for depth in args.depths])
-    print(header)
-
-    for sample in samples:
-        sample.calculate_coverage(args.depths, args.out, args.equation)
-
-    logger.info(format_str(f' Completed '.center(line_width, '='), '36'))
-    sys.exit(0)
 
 
 # ............... Functions ............... #
@@ -118,17 +74,16 @@ def parse_args(arguments):
 
 
 def get_read_sizes(read_file, gzipped):
-    # This function opens the fastq-formatted file to count the number of reads and gets the length of every
-    # read to calculate the most frequently occurring insert size.
-    # It's probably the slowest part of the program, and pure-python optimisations are encouraged.
+    # This function opens the fastq-formatted file to count the number of reads and gets the length of every read
+    # -1 removes newline char, islice iterates over 2nd line in steps of 4
     start = time()
-    openfile = gzip.open(read_file, 'rb') if gzipped else open(read_file, 'rb')
-    # Size is the most common insert size, however this is slower as requires collection of all seq lengths
-    # islice iterates over every 2nd line in steps of 4
-    # -1 removes newline char count
-    sizes = [len(line) - 1 for line in islice(openfile, 1, None, 4)]
-    openfile.close()
-    logger.debug(f"Counted reads and lengths for {read_file} [{time()-start:.2f}s]")
+    if gzipped:
+        with gzip.open(read_file, 'rb') as f:
+            sizes = [len(line) - 1 for line in islice(f, 1, None, 4)]
+    else:
+        with open(read_file, 'rb') as f:
+            sizes = [len(line) - 1 for line in islice(f, 1, None, 4)]
+    logger.debug(f"Counted reads and lengths for {read_file} {format_str(f'[{time()-start:.2f}s]', '94')}")
     return sizes
 
 
@@ -143,7 +98,7 @@ def subsample_reads(read_file, out_dir, n_reads, gzipped):
         b'\n'.join(read for read in choices(findall(b"\n".join([b"[^\n]+"] * 4), openfile.read()), k=n_reads)))
     # This does assume that the sequence lines are not wrapped and there are 4 lines per read
     openfile.close()
-    logger.debug(f"Written sub-sampled reads to {out_file} [{time()-start:.2f}s]")
+    logger.debug(f"Written sub-sampled reads to {out_file} {format_str(f'[{time()-start:.2f}s]', '94')}")
     return out_file
 
 
@@ -178,7 +133,8 @@ def process_input(forward_files, reverse_files, single_files, assembly_files, fo
         if not sample.reads:
             logger.warning(format_str(f"WARNING: No read files found for {sample.sample_name}", '93'))
         elif len(sample.reads) > 3:
-            logger.warning(format_str(f"WARNING: More than 3 files for {sample.sample_name}: {' '.join(sample.reads)}", '93'))
+            logger.warning(format_str(f"WARNING: More than 3 files for {sample.sample_name}: "
+                                      f"{' '.join(sample.reads)}", '93'))
         else:
             sample_list.append(sample)
 
@@ -200,8 +156,11 @@ def format_str(string, format_number):
 
 def get_genome_size(assembly):
     """Quickly determine genome size from fasta file"""
+    start = time()
     with open(assembly, 'rt') as f:  # Can we open in binary mode?
-        return sum([sum([len(x) for x in i.split('\n')[1:]]) for i in f.read().split('>')])
+        size = sum([sum([len(x) for x in i.split('\n')[1:]]) for i in f.read().split('>')])
+    logger.debug(f"Calculated size of {assembly} {format_str(f'[{time()-start:.2f}s]', '94')}")
+    return size
 
 
 # ............... Classes ............... #
@@ -219,47 +178,6 @@ class Sample(object):
                f'{self.genome_size}\t{self.total_reads}\t{self.total_bases}\t' \
                f'{round(self.ave_mean_read_length)}\t{round(self.ave_mode_read_length)}\t{round(self.coverage)}'
 
-    # We perform all the I/O operations here so we can do it concurrently
-    def calculate_coverage(self, subsample_depths, out_dir, equation=1):
-        self.genome_size = get_genome_size(self.assembly)
-        for read in self.reads:
-            read_sizes = get_read_sizes(read.path, read.gzipped)
-            start = time()
-            read.n_reads = len(read_sizes)
-            read.n_bases = sum(read_sizes)
-            read.mean_read_length = read.n_bases / read.n_reads
-            read.mode_read_length = max(set(read_sizes), key=read_sizes.count)
-            read.max_read_length = max(read_sizes)
-            read.min_read_length = min(read_sizes)
-            if equation == 1:
-                read.coverage += round(read.n_bases / self.genome_size)
-            elif equation == 2:
-                read.coverage += round((read.n_reads * read.mean_read_length) / self.genome_size)
-            elif equation == 3:
-                read.coverage += round((read.n_reads * read.mode_read_length) / self.genome_size)
-            logger.debug(f"Performed read size stats for {read.path} [{time() - start:.2f}s]")
-
-        start = time()
-        self.total_reads += sum([i.n_reads for i in self.reads])
-        self.total_bases += sum([i.n_bases for i in self.reads])
-        self.ave_mean_read_length += sum([i.mean_read_length for i in self.reads]) / len(self.reads)
-        self.ave_mode_read_length += sum([i.mode_read_length for i in self.reads]) / len(self.reads)
-        
-        if equation == 1:
-            self.coverage += self.total_bases / self.genome_size
-        elif equation == 2:
-            self.coverage += (self.total_reads * self.ave_mean_read_length) / self.genome_size
-        elif equation == 3:
-            self.coverage += (self.total_reads * self.ave_mode_read_length) / self.genome_size
-
-        logger.debug(f"Calculated genome coverage for {self.sample_name} [{time() - start:.2f}s]")
-
-        for read in self.reads:
-            if subsample_depths:
-                for depth in subsample_depths:
-                    read.subsamples.append(Subsample(self.coverage, depth, read, out_dir))
-            print(f'{self.get_output_string()}\t{read.get_output_string()}')
-
 
 class ReadFile(object):
     def __init__(self, filepath, extension, read_type):
@@ -268,7 +186,7 @@ class ReadFile(object):
         self.read_type = read_type
         self.gzipped = True if extension.endswith('.gz') else False
         self.n_reads, self.mean_read_length, self.mode_read_length, self.n_bases, \
-        self.min_read_length, self.coverage, self.max_read_length = [0] * 7
+        self.min_read_length, self.coverage, self.max_read_length, self.genome_size = [0] * 8
         self.subsamples = []
 
     def get_output_string(self):
@@ -277,6 +195,24 @@ class ReadFile(object):
         for s in self.subsamples:
             output_string += f'\t{s.n_subsampled_reads}'
         return output_string
+
+    def count_bases(self, equation):
+        read_sizes = get_read_sizes(self.path, self.gzipped)
+        # Start timing here
+        start = time()
+        self.n_reads = len(read_sizes)
+        self.n_bases = sum(read_sizes)
+        self.mean_read_length = self.n_bases / self.n_reads
+        self.mode_read_length = max(set(read_sizes), key=read_sizes.count)
+        self.max_read_length = max(read_sizes)
+        self.min_read_length = min(read_sizes)
+        if equation == 1:
+            self.coverage += round(self.n_bases / self.genome_size)
+        elif equation == 2:
+            self.coverage += round((self.n_reads * self.mean_read_length) / self.genome_size)
+        elif equation == 3:
+            self.coverage += round((self.n_reads * self.mode_read_length) / self.genome_size)
+        logger.debug(f"Performed read size stats for {self.path} {format_str(f'[{time()-start:.2f}s]', '94')}")
 
 
 class Subsample(object):
@@ -295,6 +231,80 @@ class Subsample(object):
                                             self.n_subsampled_reads, read.gzipped)
 
 
+# ............... Main Program ............... #
 if __name__ == "__main__":
-    main()
+    args = parse_args(sys.argv[1:])
+    # args = parse_args("-a /Users/tom/Bioinformatics/submerse/R-AJ101A_S62_L002.fasta
+    # -1 /Users/tom/Bioinformatics/submerse/R-AJ101A_S62_L002_1.fastq.gz
+    # -2 /Users/tom/Bioinformatics/submerse/R-AJ101A_S62_L002_2.fastq.gz -f".split())
+
+    if not any([args.forward, args.reverse, args.single]):
+        quit_with_error('No fastq files provided')
+
+    if args.force and len(args.assembly) not in [len(i) for i in [args.forward, args.reverse, args.single] if i]:
+        quit_with_error(
+            'The --force argument requires the number of reads in at least one of the read sets to be the '
+            'same as the number of assemblies')
+
+    if args.out and not args.depths:
+        quit_with_error('Can only output sub-sampled reads at selected depths with the -d option')
+
+    if args.out and not args.random_seed:
+        quit_with_error('Can only randomly sub-sample reads with a random seed, try -r 100')
+
+    seed(args.random_seed)  # Set random seed using the args
+
+    line_width = 70  # Nice general terminal width
+    logging.basicConfig(format="[%(asctime)s %(levelname)s %(funcName)s] %(message)s",
+                        datefmt='%H:%M:%S', level=args.loglevel)
+    logger.info(format_str(f' {path.basename(__file__)} {__version__} '.center(line_width, '='), '36'))
+    logger.info(f'Platform: {sys.platform}')
+
+    if sys.version_info[0] < 3:
+        quit_with_error(f"Python version needs to be 3, not {sys.version_info[0]}")
+    elif sys.version_info[1] < 9:
+        quit_with_error(f"Python version needs to be 3.9, not {sys.version_info[0]}.{sys.version_info[1]}")
+
+    logger.info(f'Python: {sys.version_info[0]}.{sys.version_info[1]}')
+
+    samples = process_input(args.forward, args.reverse, args.single, args.assembly, args.force)
+
+    header = f'Sample\tAssembly_file\tGenome_size\tTotal_reads\tTotal_bases\tAve_mean_read_length\tAve_mode_read_length\t' \
+             f'Total_coverage\tRead_file\tMean_read_length\tMode_read_length\tRead_coverage\tMax_read_length\t' \
+             f'Min_read_length\tN_reads\tN_bases'
+    if args.depths:
+        header += '\t' + "\t".join([f"N_reads_at_{depth}X_depth" for depth in args.depths])
+    print(header)
+
+    for sample in samples:
+        # We perform all the I/O operations here so we can do it concurrently
+        sample.genome_size = get_genome_size(sample.assembly)
+        for read in sample.reads:
+            read.genome_size = sample.genome_size
+
+        with ProcessPoolExecutor(os.cpu_count()) as executor:
+            [executor.submit(read.count_bases(args.equation)) for read in sample.reads]
+
+        sample.total_reads += sum([i.n_reads for i in sample.reads])
+        sample.total_bases += sum([i.n_bases for i in sample.reads])
+        sample.ave_mean_read_length += sum([i.mean_read_length for i in sample.reads]) / len(sample.reads)
+        sample.ave_mode_read_length += sum([i.mode_read_length for i in sample.reads]) / len(sample.reads)
+
+        if args.equation == 1:
+            sample.coverage += sample.total_bases / sample.genome_size
+        elif args.equation == 2:
+            sample.coverage += (sample.total_reads * sample.ave_mean_read_length) / sample.genome_size
+        elif args.equation == 3:
+            sample.coverage += (sample.total_reads * sample.ave_mode_read_length) / sample.genome_size
+
+        for read in sample.reads:
+            if args.depths:
+                for depth in args.depths:
+                    read.subsamples.append(Subsample(sample.coverage, depth, read, args.out))
+            print(f'{sample.get_output_string()}\t{read.get_output_string()}')
+
+    logger.info(format_str(f' Completed '.center(line_width, '='), '92'))
+    sys.exit(0)
+
+
 
